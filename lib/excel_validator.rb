032a940771c5b6e033f619dcc1b922c36e9992e6
merakissb <mbustosquiroz@gmail.com>
2021-03-17 01:45:33 -0300

    fixed validate_guardian method

--- lib/excel_validator.rb
+++ lib/excel_validator.rb
@@ -1,6 +1,6 @@
 module ExcelValidator
   OUTPUT_FILE_SHEETS_NAMES = ["Reporte Estudiantes", "CEL AP2 = AP1", "RUT INVALIDOS DE ESTUDIANTES"]
-  FIRST_GUARDIAN_PHONE_NUMBER_RANGE = 0..33
+  INVALID_DATA_SECOND_GUARDIAN = 39..45
   
 
   def self.process(file)
@@ -22,26 +22,36 @@ module ExcelValidator
   private_class_method def self.validate_guardians(sheet)
     @first_guardian_phones = sheet.column(@headers["Celular Apoderado"] + 1)
     @second_guardian_phones = sheet.column(@headers["Celular Apoderado Secundario"] + 1) 
-
+    main_sheet = find_sheet(@output_file, "Reporte Estudiantes")
+    error_sheet = find_sheet(@output_file, "CEL AP2 = AP1")
     @first_guardian_phones.each_with_index do |first_guardian_phone, index|
       next if index.zero?
+      
+      @second_guardian_phones.each_with_index do |second_guardian_phone, index|
+        next if index.zero?
 
-      @second_guardian_phones.each do |second_guardian_phone|
-        if first_guardian_phone == second_guardian_phone and first_guardian_phone != nil
+        row = sheet.row(index + 1)        
+        
+        if first_guardian_phone == second_guardian_phone && first_guardian_phone != nil
           
-          row = sheet.row(index + 1)
-          row2 = row[FIRST_GUARDIAN_PHONE_NUMBER_RANGE]
-          worksheet = find_sheet(@output_file, "CEL AP2 = AP1")
-          worksheet.add_row(row)
+          INVALID_DATA_SECOND_GUARDIAN.each do |range|
+            row[range - 1] = nil
+          end
+
+          error_sheet.add_row(row)
+          main_sheet.add_row(row)
+          @second_guardian_phones[index] = nil
+        else
+          main_sheet.add_row(row)
         end
       end
     end
   end
 
   private_class_method def self.copy_base_sheet
-    @sheet.rows.each do |row|
-      output_sheet = find_sheet(@output_file, "Reporte Estudiantes")
-      output_sheet.add_row(row)
+    output_sheet = find_sheet(@output_file, "Reporte Estudiantes")
+    @sheet.parse(headers: true) do |row|
+      output_sheet.add_row(row.values)
     end
   end
 
